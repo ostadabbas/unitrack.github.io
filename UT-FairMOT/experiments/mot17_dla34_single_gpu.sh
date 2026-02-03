49c47ebdad78ec84be67ba186d5a052b585bc003
bishoymoussas <galoaa.b@northeastern.edu>
2026-02-03 11:37:17 -0500

    UniTrack supplementary material: website with authors, badges, and comparison results

--- UT-FairMOT/experiments/mot17_dla34_single_gpu.sh
+++
@@ -1,9 +0,0 @@
-cd src
-python train.py mot \
-    --exp_id mot17_dla34 \
-    --load_model '../models/ctdet_coco_dla_2x.pth' \
-    --data_cfg '../src/lib/cfg/mot17.json' \
-    --gpus 0 \
-    --batch_size 6 \
-    --master_batch_size 6
-cd ..
--- UT-Trackformer/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='trackformer',
-      packages=['trackformer'],
-      package_dir={'':'src'},
-      version='0.0.1',
-      install_requires=[],)
